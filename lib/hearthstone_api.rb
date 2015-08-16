14ba14d5914820643129b2cea2d974f6a376d0cc
Bryan Mytko <bryanmytko@gmail.com>
2015-08-15 23:11:59 -0400

    Require new modules

--- lib/hearthstone_api.rb
+++ lib/hearthstone_api.rb
@@ -1,7 +1,13 @@
 require "httparty"
-require "hearthstone_api/version"
+require "dotenv"
+
+Dotenv.load
+
+require "hearthstone_api/base_model"
 require "hearthstone_api/configuration"
 require "hearthstone_api/connection"
+require "hearthstone_api/cards"
+require "hearthstone_api/version"
 
 module HearthstoneApi
   include HTTParty
