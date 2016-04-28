c7d4849c2870f4a9290c84c5bce3dafdd29c5ed5
Bryan Mytko <bryanmytko@gmail.com>
2016-04-28 17:53:57 -0400

    Add cards options spec

--- spec/hearthstone_api/cards_spec.rb
+++ spec/hearthstone_api/cards_spec.rb
@@ -2,10 +2,19 @@ require "spec_helper"
 
 describe HearthstoneApi::Cards do
   let(:cards) { described_class.new }
+  let(:option) { "attack" }
 
   it "returns cards" do
     VCR.use_cassette("hearthstone_api/cards") do
       expect(cards.all).to_not be_empty
+    end
+  end
+
+  it "accepts options" do
+    VCR.use_cassette("hearthstone_api/cards/options") do
+      query = cards.all(option => 1)
+      option_values = query[query.keys.first].collect{ |c| c[option] }.uniq
+      expect(option_values).to eq([1])
     end
   end
 end
