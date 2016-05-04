226804a146f5c6edfcf001d6dbb89aeec3a86ba3
Bryan Mytko <bryanmytko@gmail.com>
2016-05-04 15:33:46 -0400

    Add Cardbacks API; Resolve #1

--- lib/hearthstone_api.rb
+++ lib/hearthstone_api.rb
@@ -4,12 +4,12 @@ require "dotenv"
 Dotenv.load
 
 require "hearthstone_api/base_model"
+require "hearthstone_api/cardbacks"
+require "hearthstone_api/cards"
 require "hearthstone_api/configuration"
 require "hearthstone_api/connection"
 require "hearthstone_api/exceptions"
 require "hearthstone_api/version"
-require "hearthstone_api/cards"
-require "hearthstone_api/cardbacks"
 
 module HearthstoneApi
   include HTTParty
