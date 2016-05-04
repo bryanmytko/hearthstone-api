26bb8656052227ad18c34e1d6427bb78c4c037b2
Bryan Mytko <bryanmytko@gmail.com>
2016-05-04 15:20:45 -0400

    Require cardbacks module

--- lib/hearthstone_api.rb
+++ lib/hearthstone_api.rb
@@ -6,9 +6,10 @@ Dotenv.load
 require "hearthstone_api/base_model"
 require "hearthstone_api/configuration"
 require "hearthstone_api/connection"
-require "hearthstone_api/cards"
-require "hearthstone_api/version"
 require "hearthstone_api/exceptions"
+require "hearthstone_api/version"
+require "hearthstone_api/cards"
+require "hearthstone_api/cardbacks"
 
 module HearthstoneApi
   include HTTParty
