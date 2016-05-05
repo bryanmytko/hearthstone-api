6f06f0213e36ff3b4f71d6a210d18f4e4d22b1d8
Bryan Mytko <bryanmytko@gmail.com>
2016-05-05 10:30:03 -0400

    Add Cards#type API

--- lib/hearthstone_api/cards.rb
+++ lib/hearthstone_api/cards.rb
@@ -4,12 +4,16 @@ module HearthstoneApi
       get "/cards", options
     end
 
+    def self.single(name_or_id, options = {})
+      get "/cards/#{name_or_id}", options
+    end
+
     def self.classes(klass, options = {})
       get "/cards/classes/#{klass.capitalize}", options
     end
 
-    def self.single(name_or_id, options = {})
-      get "/cards/#{name_or_id}", options
+    def self.type(type, options = {})
+      get "/cards/types/#{type.capitalize}", options
     end
   end
 end
